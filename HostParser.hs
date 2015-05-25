d751ff09e877562aec24ab74ec600c36a74ac502
Homam <bluesnowball@gmail.com>
2015-05-25 16:29:34 +0400

    complete host parser

--- HostParser.hs
+++ HostParser.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE BangPatterns #-}
+
 import System.IO  
 import Text.Parsec (ParseError)
 import Text.Parsec.String (Parser)
 import Text.Parsec.String.Parsec (try)
 import Text.Parsec.String.Char
 import Text.Parsec.String.Combinator 
-import Control.Applicative ((<$>), (<*>), (<$), (<*), (*>), (<|>), many)
-import Control.Monad (void, ap, mzero)
-import Data.Char (isLetter, isDigit)
+import Control.Applicative --((<$>), (<*>), (<$), (<*), (*>), (<|>), many)
+import Control.Monad --(void, ap, mzero)
+-- import Data.Char (isLetter, isDigit)
 import FunctionsAndTypesForParsing
-import Data.List (intersperse, isInfixOf)
+import Data.List (intercalate) --, isInfixOf)
 import System.Environment (getArgs)
+
 
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \n\t"
@@ -18,26 +21,59 @@ lexeme :: Parser a -> Parser a
 lexeme p = p <* whitespace
  
 main :: IO () 
-main = do  
-    inOrOut <- getArgs >>= return <$> (\x -> if x == "in" then InOffice else OutOffice) . head
-    handle <- openFile "host" ReadMode  
-    contents <- hGetContents handle  
-    writeHostFile $ parseWithEof document contents >>= return <$> foldr1 (\a b -> a ++ "\n" ++ b) . map (toString . transform inOrOut) 
-    hClose handle  
+main = getArgs >>= execArgs . parseArgs
+    where
+        execArgs (Left e) = print e
+        execArgs (Right (Args inOrOut fileName)) = execute fileName inOrOut
 
+data InOrOut = InOffice | OutOffice deriving Show
+data Args = Args InOrOut String
 
-writeHostFile :: Either ParseError String -> IO ()
-writeHostFile (Left e) = print $ show e
-writeHostFile (Right content) = do
-    handle <- openFile "./hostw" WriteMode
-    hPutStr handle content
-    hClose handle  
+instance Show Args where
+    show (Args inOurOut fileName) = show inOurOut ++ " " ++ fileName
+
+parseArgs :: [String] -> Either String Args
+parseArgs [inOurOut, fileName] = combine inOurOut'
+    where 
+        inOurOut'
+          | "in" == inOurOut = Right InOffice
+          | "out" == inOurOut = Right OutOffice
+          | otherwise = Left "Expected in or out"
+        combine (Left e) = Left e
+        combine (Right inOrOut'') = Right $ Args inOrOut'' fileName
+
+parseArgs _  = Left "Invalid arguments"
+
+execute :: String -> InOrOut -> IO ()
+execute fileName inOrOut = do
+    contents <- readTextFile fileName
+    writeHostFile fileName $ parseWithEof document contents >>= return <$> intercalate "\n" . map (show . transform inOrOut) 
+
+readTextFile :: String -> IO String
+readTextFile fileName = do
+    handle <- openFile fileName ReadMode  
+    contents <- hGetContents handle >>= \x -> return $! x
+    hClose handle
+    return contents
+
+writeTextFile :: String -> String -> IO ()
+writeTextFile fileName !contents = do
+    handle <- openFile fileName WriteMode
+    hPutStr handle contents
+    hClose handle      
+
+writeHostFile :: String -> Either ParseError String -> IO ()
+writeHostFile _ (Left e) = print $ show e
+writeHostFile fileName (Right !content) = writeTextFile fileName content
 
 
 data Line = Empty | Comment String | Command String String
-    deriving Show
 
-data InOrOut = InOffice | OutOffice
+instance Show Line where
+    show Empty = ""
+    show (Comment a) = '#' : a
+    show (Command a b) = a ++ " " ++ b
+
 
 transform :: InOrOut -> Line -> Line
 transform inOrOut (Command a b) = 
@@ -49,18 +85,12 @@ transform inOrOut (Command a b) =
         getParams OutOffice = reverse $ getParams InOffice
 transform _ a = a
 
-toString :: Line -> String
-toString Empty = ""
-toString (Comment a) = '#' : a
-toString (Command a b) = a ++ " " ++ b
-
-
 
 document :: Parser [Line]
-document = line `endBy` char '\n'
+document = line `sepEndBy` void (char '\n')
 
 line :: Parser Line
-line = (eof >> return Empty) <|> comment <|> command
+line = try comment <|> try command <|> emptyLine
 
 comment :: Parser Line
 comment = Comment <$> do
@@ -72,12 +102,15 @@ command :: Parser Line
 command = Command <$> ip <*> host
     where 
         ip = lexeme (try ip4 <|> ip6)
-        host = many (noneOf "\n")
+        host = many1 (noneOf "\n")
+
+emptyLine :: Parser Line
+emptyLine = const Empty <$> void (many1 (oneOf "\t ") <|> string "")
 
 ip4 :: Parser String
 ip4 = do
     ips <- ipdigit `sepBy1` char '.'
-    return $ foldr1 (\a b -> a ++ "." ++ b) ips
+    return $ intercalate "." ips
     where 
         ipdigit = do 
             i <- many1 digit
@@ -85,8 +118,8 @@ ip4 = do
                 if j >= 0 && j < 256 then return i else fail "Expected a number less than 256"
 
 ip6 :: Parser String
-ip6 = do
+ip6 = string "::1" <|> do
     ips <- ipdigit `sepBy1` string "::"
-    return $ foldr1 (\a b -> a ++ "::" ++ b) ips
+    return $ intercalate "::" ips
     where
-        ipdigit = many (noneOf "\t: ")
+        ipdigit = many1 (noneOf "\n\t: ")
